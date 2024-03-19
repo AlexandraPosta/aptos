cd7bb440d8d72d92a6037631999f621c7b8247d5
Alexandra Posta <posta_alex222@yahoo.com>
2024-03-19 16:36:08 +0000

    feat(LQR): include first iteration of the LQR Controller

---
+++ avionics_firmware/drivers/SERVO_driver.c
@@ -0,0 +1,1 @@
+#include "SERVO_driver.h"
---
+++ avionics_firmware/drivers/SERVO_driver.h
@@ -0,0 +1,9 @@
+#ifndef SERVO_DRIVER_H
+#define SERVO_DRIVER_H
+
+#define NUM_SERVOS          4
+
+// Servo deflections for canards
+float SERVO_defs[NUM_SERVOS];
+
+#endif /* SERVO_DRIVER_H */
